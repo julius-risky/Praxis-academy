0f97b36b2a7432c8741a19f7c6993a3e4c84f60f
unknown <julius.risky@gmail.com>
2020-03-11 23:32:18 +0700

    mengganti nama file ke primary path

--- novice/01-05/kasus/json.py
+++ novice/01-05/kasus/json.py
@@ -2,7 +2,7 @@
 import json
 
 #buka file json
-file_json=open('userjson.json')
+file_json=open('F:\\Praxis-academy\\novice\\01-05\\kasus\\Biodata.json')
 
 #load file json dari file yang sudah dibuka
 data = json.loads(file_json.read())
