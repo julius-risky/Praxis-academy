1024f49188c2e2ead61beb6ce45f519730543659
LENOVO <julius.risky@gmail.com>
2020-03-24 22:04:59 +0700

    latihan

--- novice/03-02/latihan/hello.py
+++ novice/03-02/latihan/hello.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return '<h2>hello word!</h2>'
+
+if __name__ == "__main__":
+    application.run(debug=True)
