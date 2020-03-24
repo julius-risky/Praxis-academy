573dbfccb6bbe9025f2ec70039aa52cf0e3fa6dd
LENOVO <julius.risky@gmail.com>
2020-03-24 22:06:42 +0700

    latihan

--- novice/03-03/latihan/run.py
+++ novice/03-03/latihan/run.py
@@ -2,4 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def templatetest():
+    return render_template('templete1.html',my_string='wheeee!',my_list = [0,1,2,3,4,5])
+
+if __name__ == "__main__":
+    app.run(debug=True) 
+
 
