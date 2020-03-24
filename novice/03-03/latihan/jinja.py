0d962013ff10fbb8052870989074891de59aba6a
LENOVO <julius.risky@gmail.com>
2020-03-24 22:05:47 +0700

    latihan

--- novice/03-03/latihan/jinja.py
+++ novice/03-03/latihan/jinja.py
@@ -2,10 +2,4 @@ from jinja2 import Template
 
 def main():
     t = Template("Hello {{ something }}!")
-    t.render(something="World")
-
-    t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
-    t.render()
-
-if __name__ == "__main__":
-    main()
+  
