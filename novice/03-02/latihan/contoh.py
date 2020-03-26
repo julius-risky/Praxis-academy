aa392a0ae7ce3de5f69cdc17be4348ef523c0eed
LENOVO <julius.risky@gmail.com>
2020-03-26 23:12:53 +0700

    flask parameter

--- novice/03-02/latihan/contoh.py
+++ novice/03-02/latihan/contoh.py
@@ -5,4 +5,13 @@ app = Flask('aplikasi keren')
 @app.route('/')
 def indeks():
     return 'hello'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'hello,{name}!'    
+    
 app.run(debug=True)
