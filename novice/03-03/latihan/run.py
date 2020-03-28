461daad6e2c165fd13266c1c870ac37d145f7060
LENOVO <julius.risky@gmail.com>
2020-03-28 18:44:35 +0700

    latihan

--- novice/03-03/latihan/run.py
+++ novice/03-03/latihan/run.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def templatetest():
-    return render_template('templete1.html',my_string='wheeee!',my_list = [0,1,2,3,4,5])
+    return render_template("F:\\Praxis-academy\\novice\\03-03\latihan\\template.html",my_string='wheeee!',my_list = [0,1,2,3,4,5])
 
 if __name__ == "__main__":
     app.run(debug=True) 
+
